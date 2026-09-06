3d3c225d27d02e5377cc037c9784a4415cfdcd73
pasindu chamara <pasinduc613@gmail.com>
2026-09-06 14:09:01 +0530

    Day 06

---
+++ prisma/migrations/20260905105957_create_cource_table/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Cource" (
+    "name" TEXT NOT NULL,
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "Cource_pkey" PRIMARY KEY ("id")
+);
---
+++ prisma/migrations/20260905110630_ctreate_teachers/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Teachers" (
+    "name" TEXT NOT NULL,
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "Teachers_pkey" PRIMARY KEY ("id")
+);
